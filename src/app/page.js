d5c5e6e08dde9ed1cb247cf23c156c86b64c457e
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-04-01 21:39:02 +0500

    Second

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+'use client'
+import EventsFunctionsState from "@/components/EventsFunctionsState";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h2>Farzeen Ali</h2>
+      <EventsFunctionsState />
     </main>
   );
 }
---
+++ src/components/EventsFunctionsState.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const EventsFunctionsState = () => {
+    const [firstName, setFirstName] = useState('Ali');
+    // const sayHello = (name) => {
+    //     alert(`Hello ${name}`);
+    // }
+    // let firstName = 'Ali';
+    const changeName = () => {
+        // firstName = "Farzeen"
+        setFirstName('Farzeen');
+    }
+  return (
+    <>
+     <h2>Events Functions & State</h2> 
+     {/* <button onClick={() => alert('Hello')}>Click Me</button> */}
+     {/* <button onClick={() => sayHello('Farzeen')}>Click Me</button> */}
+     <h3>My name is: {firstName}</h3>
+     <button onClick={changeName}>Click Me</button>
+    </>
+  );
+}
+
+export default EventsFunctionsState;
