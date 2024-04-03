60b6fd5435cb3787322a59b2add30e7fb4de9c0e
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-04-03 23:08:27 +0500

    Second

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <>
+     <h2>About US</h2> 
+    </>
+  );
+}
+
+export default About;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 'use client'
-import EventsFunctionsState from "@/components/EventsFunctionsState";
+// import EventsFunctionsState from "@/components/EventsFunctionsState";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h2>Farzeen Ali</h2>
-      <EventsFunctionsState />
+      {/* <EventsFunctionsState /> */}
     </main>
   );
 }
