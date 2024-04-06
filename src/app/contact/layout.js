a0ada357e153291356e2cbbac6b81f4f953cfa44
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-04-06 21:06:48 +0500

    Common Layout

---
+++ src/app/contact/layout.js
@@ -0,0 +1,10 @@
+export default function Layout({ children }){
+    return (
+        <>
+            <h2>Common Layout For Contact</h2>
+            {
+                children
+            }
+        </>
+    )
+}
