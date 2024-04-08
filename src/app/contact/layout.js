bc93efe17a90deecc844abf1ab4ba1be78db0e47
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-04-08 21:56:14 +0500

    Conditional Layout

--- src/app/contact/layout.js
+++ src/app/contact/layout.js
@@ -1,10 +1,16 @@
+'use client';
+import { usePathname } from "next/navigation"
+
 export default function Layout({ children }){
+   const currentPathName = usePathname();
+   console.log(currentPathName);
     return (
         <>
-            <h2>Common Layout For Contact</h2>
             {
-                children
+                currentPathName !== '/contact/company' ? 
+                <h2>Common Layout For Contact</h2> : null
             }
+            { children }
         </>
     )
 }
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -6,12 +6,12 @@ const Contact = () => {
   let router = useRouter();
   return (
     <>
-     <h2>Contact Page</h2> 
-     {/* <Link href='/home'>Home Page</Link> */}
-     <Link href='/contact/employee'>Contact Employee</Link>
-     <br />
-     <br />
-     <button onClick={() => router.push('/contact/company')}>Contact Company</button>
+      <h2>Contact Page</h2> 
+      {/* <Link href='/home'>Home Page</Link> */}
+      <Link href='/contact/employee'>Contact Employee</Link>
+      <br />
+      <br />
+      <button onClick={() => router.push('/contact/company')}>Contact Company</button>
     </>
   );
 }
