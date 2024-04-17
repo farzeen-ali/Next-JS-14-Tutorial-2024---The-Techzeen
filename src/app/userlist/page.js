e1a36768703cbbd4bd6bb43063b5b517d3a24314
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-04-17 23:08:26 +0500

    Fetch Data With API in Client Component

---
+++ src/app/userlist/page.js
@@ -0,0 +1,23 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const Users = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(()=> {
+        async function getUsers () {
+            const usersData = await fetch('https://jsonplaceholder.typicode.com/users')
+            setUsers(await usersData.json())
+        }
+        getUsers();
+    }, [])
+  return (
+    <>
+     <h2>Users List | Fetch Data With API</h2> 
+     {
+        users.map((user) => <li key={user.id}>{user.username}</li>)
+     }
+    </>
+  );
+}
+
+export default Users;
