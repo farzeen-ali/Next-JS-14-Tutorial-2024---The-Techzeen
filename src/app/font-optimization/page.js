6f04470e9938ba20b8a7603320fd655dae781170
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-04-25 22:14:44 +0500

    Font Optimization

--- src/app/font-optimization/page.js
+++ src/app/font-optimization/page.js
@@ -1,9 +1,14 @@
-import React from 'react';
-
+import { Poppins } from "next/font/google";
+const poppins = Poppins({
+    weight: '900',
+    subsets: ['latin'],
+    display: 'swap'
+})
 const FontOptimization = () => {
   return (
     <>
-     <h2>Font Optimization</h2> 
+     {/* <h2 style={{fontFamily: 'Poppins', fontWeight: 300}}>Font Optimization In Next JS 14</h2>  */}
+     <h2 className={poppins.className}>Font Optimization In Next JS 14</h2>
     </>
   );
 }
