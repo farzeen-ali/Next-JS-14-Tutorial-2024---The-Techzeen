c2db2a1959b26e25016341e16cbc0732ef815f1b
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-05-14 21:58:10 +0500

    Catch All API Routes

---
+++ src/app/api/[...all]/route.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server"
+
+export const GET = async (req, value) => {
+    // console.log(value.params.all)
+    const details = value.params.all
+    // return NextResponse.json({result: "Success"})
+    return NextResponse.json({result: details})
+}
