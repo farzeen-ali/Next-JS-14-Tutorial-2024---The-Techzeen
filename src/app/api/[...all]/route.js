32aa40f9b35c80248e11016fac676840b472f0ac
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-05-15 22:39:58 +0500

    Done

--- src/app/api/[...all]/route.js
+++ src/app/api/[...all]/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 
 export const GET = async (req, value) => {
-    // console.log(value.params.all)
+    // console.log(value.params.all) 
     const details = value.params.all
     // return NextResponse.json({result: "Success"})
     return NextResponse.json({result: details})
