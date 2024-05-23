b1f728df714cf519d4f97e6f6efd483d66cf7e39
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-05-23 22:49:00 +0500

    PUT Integration With Mongo DB in Next JS 14

--- src/app/mongo-get-emp/[update]/page.js
+++ src/app/mongo-get-emp/[update]/page.js
@@ -1,8 +1,10 @@
 'use client';
 
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const UpdateEmployee = (props) => {
+    const router = useRouter();
     const [name, setName] = useState('');
     const [salary, setSalary] = useState('');
     const [department, setDepartment] = useState('');
@@ -22,13 +24,27 @@ const UpdateEmployee = (props) => {
         }
         employeeDetails();
     },[id])
+    const updateRecord = async () => {
+      let data = await fetch(`http://localhost:3000/api/db-emp/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({name, salary, department})
+      })
+      data = await data.json();
+      if (data.success){
+        alert('Record Updated Successfully')
+        router.push('/mongo-get-emp')
+      }
+      else{
+        alert('Try Again')
+      }
+    } 
   return (
     <>
      <h2>Update Employee</h2>
      <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter Employee Name" /><br /><br />
      <input type="text" value={salary} onChange={(e) => setSalary(e.target.value)} placeholder="Enter Employee Salary" /><br /><br />
      <input type="text" value={department} onChange={(e) => setDepartment(e.target.value)} placeholder="Enter Employee Department" /><br /><br />
-     <button>Submit</button>
+     <button onClick={updateRecord}>Update</button>
     </>
   );
 }
